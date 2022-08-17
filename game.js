956c3fd5a02f749a9e74c22b260ad6d154ad61df
unknown <ejmahadiair@gmail.com>
2022-08-18 02:56:10 +0600

    game copleted

--- game.js
+++ game.js
@@ -1,25 +1,29 @@
-const play = (player, oponent) => {
-  //select canvas
+//new clear
+function play(player, OPPONENT) {
+  // SELECT CANAVS
   const canvas = document.getElementById("cvs");
   const ctx = canvas.getContext("2d");
-  //
-  //Board variables
+
+  // BOARD VARIABLES
   let board = [];
   const COLUMN = 3;
   const ROW = 3;
-
   const SPACE_SIZE = 150;
 
+  // STORE PLAYER'S MOVES
   let gameData = new Array(9);
+
+  // By default the first player to play is the human
   let currentPlayer = player.man;
 
-  //load palyer images
-  const xImg = new Image();
-  xImg.src = "./img/x.png";
-  const oImg = new Image();
-  oImg.src = "./img/o.png";
+  // load X & O images
+  const xImage = new Image();
+  xImage.src = "img/x.png";
 
-  //Win Combinations
+  const oImage = new Image();
+  oImage.src = "img/o.png";
+
+  // Win combinations
   const COMBOS = [
     [0, 1, 2],
     [3, 4, 5],
@@ -31,98 +35,234 @@ const play = (player, oponent) => {
     [2, 4, 6],
   ];
 
+  // FOR GAME OVER CHECK
   let GAME_OVER = false;
-  //DRAW THE BOARD
+
+  // DRAW THE BOARD
   function drawBoard() {
+    // WE give every space a unique id
+    // So we know exactly where to put the player's move on the gameData Array
     let id = 0;
     for (let i = 0; i < ROW; i++) {
       board[i] = [];
       for (let j = 0; j < COLUMN; j++) {
         board[i][j] = id;
         id++;
+
+        // draw the spaces
         ctx.strokeStyle = "white";
         ctx.strokeRect(j * SPACE_SIZE, i * SPACE_SIZE, SPACE_SIZE, SPACE_SIZE);
       }
     }
   }
   drawBoard();
-  //On player's Click
-  canvas.addEventListener("click", () => {
-    //if the game over the game will stop
+
+  // ON PLAYER'S CLICK
+  canvas.addEventListener("click", function (event) {
+    // IF IT's A GAME OVER? EXIT
     if (GAME_OVER) return;
-    //x & Y possition of mouse click relative to the canvas
+
+    // X & Y position of mouse click relative to the canvas
     let X = event.clientX - canvas.getBoundingClientRect().x;
     let Y = event.clientY - canvas.getBoundingClientRect().y;
 
-    //calculate i and j
+    // WE CALCULATE i & j of the clicked SPACE
     let i = Math.floor(Y / SPACE_SIZE);
     let j = Math.floor(X / SPACE_SIZE);
-    //get the id of the space the player clicked on
+
+    // Get the id of the space the player clicked on
     let id = board[i][j];
 
-    //check that the space are already used or not
+    // Prevent the player to play the same space twice
     if (gameData[id]) return;
-    //store the player moves
+
+    // store the player's move to gameData
     gameData[id] = currentPlayer;
 
-    //draw the move on board
+    // draw the move on board
     drawOnBoard(currentPlayer, i, j);
 
+    // Check if the play wins
     if (isWinner(gameData, currentPlayer)) {
       showGameOver(currentPlayer);
       GAME_OVER = true;
       return;
     }
+
+    // check if it's a tie game
     if (isTie(gameData)) {
-      showGameOver("Tie");
+      showGameOver("tie");
       GAME_OVER = true;
       return;
     }
-    //give turn to t he other player
-    currentPlayer = currentPlayer == player.man ? player.friend : player.man;
+
+    if (OPPONENT == "computer") {
+      // get id of space using minimax algorithm
+      let id = minimax(gameData, player.computer).id;
+
+      // store the player's move to gameData
+      gameData[id] = player.computer;
+
+      // get i and j of space
+      let space = getIJ(id);
+
+      // draw the move on board
+      drawOnBoard(player.computer, space.i, space.j);
+
+      // Check if the play wins
+      if (isWinner(gameData, player.computer)) {
+        showGameOver(player.computer);
+        GAME_OVER = true;
+        return;
+      }
+
+      // check if it's a tie game
+      if (isTie(gameData)) {
+        showGameOver("tie");
+        GAME_OVER = true;
+        return;
+      }
+    } else {
+      // GIVE TURN TO THE OTHER PLAYER
+      currentPlayer = currentPlayer == player.man ? player.friend : player.man;
+    }
   });
 
-  const isWinner = (gameData, player) => {
+  // MINIMAX
+  function minimax(gameData, PLAYER) {
+    // BASE
+    if (isWinner(gameData, player.computer)) return { evaluation: +10 };
+    if (isWinner(gameData, player.man)) return { evaluation: -10 };
+    if (isTie(gameData)) return { evaluation: 0 };
+
+    // LOOK FOR EMTY SPACES
+    let EMPTY_SPACES = getEmptySpaces(gameData);
+
+    // SAVE ALL MOVES AND THEIR EVALUATIONS
+    let moves = [];
+
+    // LOOP OVER THE EMPTY SPACES TO EVALUATE THEM
+    for (let i = 0; i < EMPTY_SPACES.length; i++) {
+      // GET THE ID OF THE EMPTY SPACE
+      let id = EMPTY_SPACES[i];
+
+      // BACK UP THE SPACE
+      let backup = gameData[id];
+
+      // MAKE THE MOVE FOR THE PLAYER
+      gameData[id] = PLAYER;
+
+      // SAVE THE MOVE'S ID AND EVALUATION
+      let move = {};
+      move.id = id;
+      // THE MOVE EVALUATION
+      if (PLAYER == player.computer) {
+        move.evaluation = minimax(gameData, player.man).evaluation;
+      } else {
+        move.evaluation = minimax(gameData, player.computer).evaluation;
+      }
+
+      // RESTORE SPACE
+      gameData[id] = backup;
+
+      // SAVE MOVE TO MOVES ARRAY
+      moves.push(move);
+    }
+
+    // MINIMAX ALGORITHM
+    let bestMove;
+
+    if (PLAYER == player.computer) {
+      // MAXIMIZER
+      let bestEvaluation = -Infinity;
+      for (let i = 0; i < moves.length; i++) {
+        if (moves[i].evaluation > bestEvaluation) {
+          bestEvaluation = moves[i].evaluation;
+          bestMove = moves[i];
+        }
+      }
+    } else {
+      // MINIMIZER
+      let bestEvaluation = +Infinity;
+      for (let i = 0; i < moves.length; i++) {
+        if (moves[i].evaluation < bestEvaluation) {
+          bestEvaluation = moves[i].evaluation;
+          bestMove = moves[i];
+        }
+      }
+    }
+
+    return bestMove;
+  }
+
+  // GET EMPTY SPACES
+  function getEmptySpaces(gameData) {
+    let EMPTY = [];
+
+    for (let id = 0; id < gameData.length; id++) {
+      if (!gameData[id]) EMPTY.push(id);
+    }
+
+    return EMPTY;
+  }
+
+  // GET i AND j of a SPACE
+  function getIJ(id) {
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        if (board[i][j] == id) return { i: i, j: j };
+      }
+    }
+  }
+
+  // check for a winner
+  function isWinner(gameData, player) {
     for (let i = 0; i < COMBOS.length; i++) {
       let won = true;
+
       for (let j = 0; j < COMBOS[i].length; j++) {
         let id = COMBOS[i][j];
         won = gameData[id] == player && won;
       }
+
       if (won) {
         return true;
       }
     }
     return false;
-  };
-  //check tie
-  const isTie = (gameData) => {
-    let isBoardfill = true;
+  }
+
+  // Check for a tie game
+  function isTie(gameData) {
+    let isBoardFill = true;
     for (let i = 0; i < gameData.length; i++) {
-      isBoardfill = gameData[i] && isBoardfill;
+      isBoardFill = gameData[i] && isBoardFill;
     }
-    if (isBoardfill) {
+    if (isBoardFill) {
       return true;
     }
     return false;
-  };
+  }
 
-  const showGameOver = (player) => {
-    let message = player == "tie" ? "No Winner" : "The Winner is";
-    let imgSrc = `./img/${player}.png`;
-    console.log("player ", player);
+  // SHOW GAME OVER
+  function showGameOver(player) {
+    let message = player == "Tie" ? " No Winner" : `The Winner is ${player}`;
+    let imgSrc = `img/${player}.png`;
+
     gameOver.innerHTML = `
-       <h1>${message}</h1>
-       <img class="winner-img" src="/img/${player}.png" </img>
-       <div class="play-again" onclick="location.reload()">Play Again</div>
+          <h1>${message}</h1>
+          <img class="winner-img" src=${imgSrc} </img>
+          <div class="play-again" onclick="location.reload()">Play Again!</div>
       `;
+
     gameOver.classList.remove("hide");
-  };
+  }
 
-  const drawOnBoard = (player, i, j) => {
-    let img = player == "x" ? xImg : oImg;
+  // draw on board
+  function drawOnBoard(player, i, j) {
+    let img = player == "x" ? xImage : oImage;
 
-    //draw image in the position of x and y based
+    // the x,y positon of the image are the x,y of the clicked space
     ctx.drawImage(img, j * SPACE_SIZE, i * SPACE_SIZE, 150, 150);
-  };
-};
+  }
+}
